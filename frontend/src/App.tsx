2910a3858ff74d3a4082166010e2979dd3b0d886
Daniel Landsman <daniellandsmanprofessional@gmail.com>
2025-03-20 17:56:36 -0400

    Update App.tsx to add Problem View Page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ import AuthPage from "./pages/AuthPage";
 import DashBoardPage from "./pages/DashboardPage";
 import TopicPage from "./pages/TopicPage";
 import MockTestPage from "./pages/MockTestPage";
-import MyProgessPage from "./pages/MyProgressPage"
+import MyProgessPage from "./pages/MyProgressPage";
+import ProblemViewPage from "./pages/ProblemViewPage";
 
 function App() {
   return (
@@ -16,11 +17,10 @@ function App() {
         <Route path="/topic-practice" element={<TopicPage />} />
         <Route path="/mock-test" element={<MockTestPage />} />
         <Route path="/my-progress" element={<MyProgessPage />} />
+        <Route path="/problem-view" element={<ProblemViewPage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
