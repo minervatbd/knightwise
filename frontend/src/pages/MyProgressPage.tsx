629b017a8a72f4ef52baf1ce4248be518531c756
Daniel Landsman <daniellandsmanprofessional@gmail.com>
2025-03-20 17:54:18 -0400

    Update MyProgressPage.tsx

--- frontend/src/pages/MyProgressPage.tsx
+++ frontend/src/pages/MyProgressPage.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Graph from "../components/Graph";
+import HistoryTable from "../components/HistoryTable"; // Import HistoryTable component
 
 const MyProgressPage: React.FC = () => {
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center h-full">
         <h1 className="text-5xl font-bold mb-4">My Progress</h1>
-          <div className="w-5/8 border-3 border-gray-300 p-4 rounded-lg shadow">
-            <Graph />
-          </div>
+
+        {/* Graph Section */}
+        <div className="w-5/8 border-3 border-gray-300 p-4 rounded-lg shadow mb-6">
+          <Graph />
+        </div>
+
+        {/* History Table Section */}
+        <div className="w-5/8 border-3 border-gray-300 p-4 rounded-lg shadow">
+          <HistoryTable /> {/* Add the HistoryTable component here */}
+        </div>
       </div>
     </Layout>
   );
