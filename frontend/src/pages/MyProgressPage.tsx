5b441b666606a38a1fccd6c7c482299543667450
Daniel Landsman <daniellandsmanprofessional@gmail.com>
2025-03-20 12:13:59 -0400

    Update MyProgressPage.tsx
    Design update for My Progress page.

--- frontend/src/pages/MyProgressPage.tsx
+++ frontend/src/pages/MyProgressPage.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import Layout from "../components/Layout";
 import Graph from "../components/Graph";
 
-const MockTestPage: React.FC = () => {
+const MyProgressPage: React.FC = () => {
   return (
     <Layout>
-      <div className="flex justify-center items-center h-full">
-        <Graph />
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-5xl font-bold mb-4">My Progress</h1>
+          <div className="w-5/8 border-3 border-gray-300 p-4 rounded-lg shadow">
+            <Graph />
+          </div>
       </div>
     </Layout>
   );
 };
 
-export default MockTestPage;
+export default MyProgressPage;
