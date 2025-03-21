b5f31c91de7b247330e4c69ce1eec26aa611907f
minervatbd <minervatbd@gmail.com>
2025-03-21 16:05:52 -0400

    logout icon plus remove import

--- mobile_test/lib/icons.dart
+++ mobile_test/lib/icons.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'styles.dart';
 
 abstract class NavigationIcons {
   static const menu = Icon(Icons.menu);
@@ -7,4 +6,5 @@ abstract class NavigationIcons {
   static const topicSelection = Icon(Icons.chat_bubble_outline);
   static const mockTest = Icon(Icons.description_outlined);
   static const myProgress = Icon(Icons.bar_chart);
+  static const logout = Icon(Icons.logout);
 } 
