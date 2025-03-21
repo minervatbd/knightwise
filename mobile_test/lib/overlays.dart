b244e175330527795132a959550d28ee958687fa
minervatbd <minervatbd@gmail.com>
2025-03-21 16:05:02 -0400

    make the overlays into their own classes with context

--- mobile_test/lib/overlays.dart
+++ mobile_test/lib/overlays.dart
@@ -3,10 +3,17 @@ import 'styles.dart';
 import 'icons.dart';
 
 const scheme = Styles.schemeMain;
+const double topBarHeight = 70;
 
-abstract class AppBars {
-  // top bar containing a ucf logo and a button opening the dropdown menu
-  static final topBar = AppBar(
+// top bar containing a ucf logo and a button opening the dropdown menu
+class TopBarMenu extends StatelessWidget implements PreferredSizeWidget {
+  const TopBarMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
     backgroundColor: Styles.schemeMain.primary,
     leading: Container(
         padding:EdgeInsets.all(8),
@@ -27,95 +34,114 @@ abstract class AppBars {
             icon: NavigationIcons.menu,
             tooltip: 'Show menu',
             onPressed: () {
-              print("show menu");
+              var state = Scaffold.of(context);
+              if (state.isDrawerOpen) {
+                state.closeDrawer();
+              } else {
+                state.openDrawer();
+              }
             }
           )
         )
       )
     ]
-  );
-  // bottom bar containing four buttons
-  static final bottomBar = BottomAppBar(
-    color: Styles.schemeMain.primary,
-    child: IconTheme(
-      data: IconThemeData(color: Styles.schemeMain.primary),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Ink(
-              decoration: ShapeDecoration(
-                color: scheme.secondary,
-                shape: CircleBorder(),
-              ),
-              // dashboard button
-              child: IconButton(
-                color: scheme.primary,
-                icon: NavigationIcons.dashboard,
-                tooltip: 'Button1',
-                onPressed: () {
-                  print("Button1");
-                }
+        );
+  }
+  
+  @override
+  Size get preferredSize => Size.fromHeight(topBarHeight);
+}
+
+// bottom bar containing four buttons
+class BottomBarMenu extends StatelessWidget {
+  const BottomBarMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Styles.schemeMain.primary,
+      child: IconTheme(
+        data: IconThemeData(color: Styles.schemeMain.primary),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: scheme.secondary,
+                  shape: CircleBorder(),
+                ),
+                // dashboard button
+                child: IconButton(
+                  color: scheme.primary,
+                  icon: NavigationIcons.dashboard,
+                  tooltip: 'Button1',
+                  onPressed: () {
+                    print("Button1");
+                  }
+                )
               )
-            )
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Ink(
-              decoration: ShapeDecoration(
-                color: scheme.secondary,
-                shape: CircleBorder(),
-              ),
-              // topic selection button
-              child: IconButton(
-                color: scheme.primary,
-                icon: NavigationIcons.topicSelection,
-                tooltip: 'Button2',
-                onPressed: () {
-                  print("Button2");
-                }
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: scheme.secondary,
+                  shape: CircleBorder(),
+                ),
+                // topic selection button
+                child: IconButton(
+                  color: scheme.primary,
+                  icon: NavigationIcons.topicSelection,
+                  tooltip: 'Button2',
+                  onPressed: () {
+                    print("Button2");
+                  }
+                )
               )
-            )
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Ink(
-              decoration: ShapeDecoration(
-                color: scheme.secondary,
-                shape: CircleBorder(),
-              ),
-              // mock test button
-              child: IconButton(
-                color: scheme.primary,
-                icon: NavigationIcons.mockTest,
-                tooltip: 'Button3',
-                onPressed: () {
-                  print("Button3");
-                }
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: scheme.secondary,
+                  shape: CircleBorder(),
+                ),
+                // mock test button
+                child: IconButton(
+                  color: scheme.primary,
+                  icon: NavigationIcons.mockTest,
+                  tooltip: 'Button3',
+                  onPressed: () {
+                    print("Button3");
+                  }
+                )
               )
-            )
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Ink(
-              decoration: ShapeDecoration(
-                color: scheme.secondary,
-                shape: CircleBorder(),
-              ),
-              // my progress button
-              child: IconButton(
-                color: scheme.primary,
-                icon: NavigationIcons.myProgress,
-                tooltip: 'Button4',
-                onPressed: () {
-                  print("Button4");
-                }
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: scheme.secondary,
+                  shape: CircleBorder(),
+                ),
+                // my progress button
+                child: IconButton(
+                  color: scheme.primary,
+                  icon: NavigationIcons.myProgress,
+                  tooltip: 'Button4',
+                  onPressed: () {
+                    print("Button4");
+                  }
+                )
               )
-            )
-          ),
-        ],
+            ),
+          ],
+        )
       )
-    )
-  );
+    );
+  }
 }
