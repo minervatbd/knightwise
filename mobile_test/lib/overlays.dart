e9c46834e173130ba0bd41066352edc5177773b1
minervatbd <minervatbd@gmail.com>
2025-03-24 17:39:38 -0400

    move question bar to overlays (we should refactor the overlays file, its getting too big)

--- mobile_test/lib/overlays.dart
+++ mobile_test/lib/overlays.dart
@@ -223,3 +223,91 @@ final oldIconTheme = IconTheme(
     ],
   )
 );
+
+class QuestionBarMenu extends StatefulWidget {
+  const QuestionBarMenu({
+    super.key,
+    required this.problemCount,
+    this.currentPageIndex = 0,
+    required this.changeIndex,
+  });
+
+  final int problemCount;
+  final int currentPageIndex;
+  final ValueChanged<int> changeIndex;
+
+  @override
+  State<QuestionBarMenu> createState() => _QuestionBarMenuState();
+}
+
+class _QuestionBarMenuState extends State<QuestionBarMenu> {
+  bool _previousEnabled = false;
+  bool _nextEnabled = true;
+  bool _submitEnabled = false;
+
+  int index = 0;
+
+  void switchButtons () {
+    if (index == widget.problemCount - 1) {
+      _nextEnabled = false;
+      _submitEnabled = true;
+    } else {
+      _nextEnabled = true;
+    }
+    if (index == 0) {
+      _previousEnabled = false;
+    } else {
+      _previousEnabled = true;
+    }
+  }
+
+  void handlePreviousButton () {
+    index--;
+
+    setState(() {
+      switchButtons();
+      widget.changeIndex(index);
+    });
+  }
+
+  void handleNextButton () {
+    index++;
+
+    setState(() {
+      switchButtons();
+      widget.changeIndex(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: scheme.primary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // previous
+          ElevatedButton(
+            style: Styles.yellowButtonStyle,
+            onPressed: _previousEnabled ? handlePreviousButton : null,
+            child: NavigationIcons.previous
+          ),
+          // submit quiz
+          ElevatedButton(
+            style: Styles.yellowButtonStyle,
+            onPressed: _submitEnabled?() {
+              Navigator.pop(context);
+            }: null,
+            child: Text("End")
+          ),
+          // next
+          ElevatedButton(
+            style: Styles.yellowButtonStyle,
+            onPressed: _nextEnabled ? handleNextButton : null,
+            child: NavigationIcons.next
+          ),
+        ],
+      )
+    );
+  }
+}
