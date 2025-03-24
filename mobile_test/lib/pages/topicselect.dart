3d336b741f7c64c234f66294dac1884567d3ca23
minervatbd <minervatbd@gmail.com>
2025-03-24 16:55:49 -0400

    implement questions sequence for testing

--- mobile_test/lib/pages/topicselect.dart
+++ mobile_test/lib/pages/topicselect.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_test/pages/questionbody.dart';
+import 'package:sprintf/sprintf.dart';
+import 'package:mobile_test/models.dart';
+import 'package:mobile_test/pages/questionsequence.dart';
 import '../styles.dart';
+
+const int testProblemCount = 10;
+
+// generates a list of dummy problems for test purposes
+List<Problem> generateDummyProblems(int count) {
+  List<Problem> problemsOut = List<Problem>.empty(growable: true);
+
+  int x = 0;
+  while (x < count) {
+    problemsOut.add(Problem(
+      "id",
+      "exam_id",
+      "section",
+      "category",
+      "subcategory",
+      sprintf("question%d", [count]),
+      "answersCorrect",
+      "answersWrong",
+    ));
+  }
+
+  return problemsOut;
+}
+
+// returns a list filled with default statuses
+List<QuestionBodyStatus> generateStatusList(int count) {
+  return List<QuestionBodyStatus>.filled(
+    count,
+    QuestionBodyStatus(false)
+  );
+}
 
 class TopicSelectPage extends StatefulWidget {
   const TopicSelectPage({super.key});
@@ -12,8 +47,27 @@ class _TopicSelectPageState extends State<TopicSelectPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("select topic dawg",
-        style: Styles.buttonTextStyle,)
+      child: ListView(
+        children: <Widget>[
+          Text(
+            "select topic",
+            style: Styles.buttonTextStyle,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => QuestionSequence(problemCount: testProblemCount, problemList: generateDummyProblems(testProblemCount), statusList: generateStatusList(testProblemCount))),
+              );
+            },
+            style: Styles.yellowButtonStyle,
+            child: Text(
+              "Start",
+              style: Styles.buttonTextStyle,
+            )
+          )
+        ]
+      )
     );
   }
 }
