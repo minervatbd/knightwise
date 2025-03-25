194bcd00909904588cb13d7272a93d57c1d459c2
YoniWorks <yeshin881@gmail.com>
2025-03-25 13:16:19 -0400

    update to responsive design and collapes sidebar on small screen

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import logo from "../assets/ucflogo.png";
+import { Menu } from "lucide-react";
+import { useLocation } from "react-router-dom";
 
-const Header: React.FC = () => {
+type HeaderProps = {
+  onMenuClick?: () => void;
+};
+
+const Header: React.FC<HeaderProps> = ({ onMenuClick }) => {
+
+  // collapse sidebar on small screens 
+  // but hide sidebar when logging in/signing up
+  const location = useLocation();
+  const hideMenu = location.pathname === "/";
+
   return (
     <header className="bg-black text-yellow-400 py-4 px-6 flex items-center justify-between">
       <div className="flex items-center space-x-4">
+        {!hideMenu && (
+          <button className="md:hidden mr-2" onClick={onMenuClick}>
+            <Menu size={28} />
+          </button>
+        )}
         <img src={logo} alt="UCF Logo" className="w-12 h-12" />
-        <h1 className="text-lg font-bold">UNIVERSITY OF CENTRAL FLORIDA</h1>
+        <h1 className="text-lg font-bold hidden sm:block">
+          UNIVERSITY OF CENTRAL FLORIDA
+        </h1>
       </div>
     </header>
   );
 };
 
 export default Header;
+
+
