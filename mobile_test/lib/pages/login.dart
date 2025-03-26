556419fa60fa3205b2ab6ef2f5f4bcf76de9e0b6
AmaryllisGreen <daytonchawk@gmail.com>
2025-03-26 00:46:08 -0400

    Update login.dart
    made it so it actually works

--- mobile_test/lib/pages/login.dart
+++ mobile_test/lib/pages/login.dart
@@ -13,6 +13,20 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+
+  @override
+  void initState(){
+    super.initState();
+
+    usernameController.addListener(() => setState(() {}));
+    passwordController.addListener(() => setState(() {}));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,104 +34,109 @@ class _LoginPageState extends State<LoginPage> {
       bottomNavigationBar: BottomBarBlank(),
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 45.0,
-            children: <Widget>[
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 45.0,
+          children: <Widget>[
 
-              //LOGIN stripe
-              Material(
-                elevation: 4,
-                child: Container(
-                  color: Styles.schemeMain.secondary,
-                  width: 412,
-                  height: 89,
-                  child: Center(
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Mulish',
-                      ),
+          //LOGIN stripe
+            Material(
+              elevation: 4,
+              child: Container(
+                color: Styles.schemeMain.secondary,
+                width: 412,
+                height: 89,
+                child: Center(
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Styles.schemeMain.primary,
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Mulish',
                     ),
                   ),
                 ),
               ),
-              
-              //textboxes and button
-              Form(
-                child: Column(
-                  spacing: 40,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    //username textbox
-                    Container(
-                      color: Colors.grey[300],
-                      padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
-                      width: 336,
-                      height: 65,
-                      child: TextField(
-                        controller: usernameController,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          hintText: 'Username',
-                          hintStyle: Styles.fieldTextStyle,
-                          border: InputBorder.none,
-                        ),
+            ),
+
+            //textboxes and button
+            Form(
+              child: Column(
+                spacing: 40,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  //username textbox
+                  Container(
+                    color: Colors.grey[300],
+                    padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
+                    width: 336,
+                    height: 65,
+                    child: TextField(
+                      controller: usernameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: Styles.fieldTextStyle,
+                        border: InputBorder.none,
                       ),
                     ),
+                  ),
 
-                    //password textbox
-                    Container(
-                      color: Colors.grey[300],
-                      padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
-                      width: 336,
-                      height: 65,
-                      child: TextField(
-                        controller: passwordController,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          hintStyle: Styles.fieldTextStyle,
-                          border: InputBorder.none,
-                          suffixIcon: IconButton(
-                            icon: isPasswordVisible
+                  //password textbox
+                  Container(
+                    color: Colors.grey[300],
+                    padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
+                    width: 336,
+                    height: 65,
+                    child: TextField(
+                      controller: passwordController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: Styles.fieldTextStyle,
+                        border: InputBorder.none,
+                        suffixIcon: IconButton(
+                          icon: isPasswordVisible
                               ? Icon(Icons.visibility_off)
                               : Icon(Icons.visibility),
-                            onPressed: () =>
-                              setState(() => isPasswordVisible = !isPasswordVisible),
-                          ),
+                          color: Styles.schemeMain.primary,
+                          onPressed: () =>
+                              setState(() =>
+                              isPasswordVisible = !isPasswordVisible),
                         ),
-                        obscureText: isPasswordVisible,
                       ),
+                      obscureText: isPasswordVisible,
                     ),
+                  ),
 
-                    //Sign in button
-                    MaterialButton(
-                      onPressed: () {
-                        //temp solution; ideally would be passed as json for login endpoint or something like that
-                        print('username: ${usernameController.text}');
-                        print('password: ${passwordController.text}');
-                        
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const HomePage()),
-                        );
-                      },
-                      elevation: 4,
-                      color: Styles.schemeMain.secondary,
-                      shape: Styles.buttonShape,
-                      height: 70,
-                      minWidth: 350,
-                      child: const Text(
-                        'Sign In',
-                        style: Styles.buttonTextStyle,
-                      ),
+                  //Sign in button
+                  MaterialButton(
+                    onPressed: () {
+                      //temp solution; ideally would be passed as json for login endpoint or something like that
+                      print('username: ${usernameController.text}');
+                      print('password: ${passwordController.text}');
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const HomePage()),
+                      );
+                    },
+                    elevation: 4,
+                    color: Styles.schemeMain.secondary,
+                    shape: Styles.buttonShape,
+                    height: 70,
+                    minWidth: 350,
+                    child: const Text(
+                      'Sign In',
+                      style: Styles.buttonTextStyle,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+            ),
+          ],
+        ),
 
         /*
         child: Container(
@@ -135,8 +154,10 @@ class _LoginPageState extends State<LoginPage> {
           )
         ),
         */
-              
+
       ),
     );
+  }
+}
   }
 }
