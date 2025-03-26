16f3bb6814b90beef9600faafc9f770bf9736d8a
AmaryllisGreen <daytonchawk@gmail.com>
2025-03-25 23:29:03 -0400

    Update login.dart
    Changes Login page to have login stripe, username and password text fields and sign in button (which is basically the login button from before)

--- mobile_test/lib/pages/login.dart
+++ mobile_test/lib/pages/login.dart
@@ -19,6 +19,107 @@ class _LoginPageState extends State<LoginPage> {
       appBar: TopBarBlank(),
       bottomNavigationBar: BottomBarBlank(),
       body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 45.0,
+            children: <Widget>[
+
+              //LOGIN stripe
+              Material(
+                elevation: 4,
+                child: Container(
+                  color: Styles.schemeMain.secondary,
+                  width: 412,
+                  height: 89,
+                  child: Center(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Mulish',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              
+              //textboxes and button
+              Form(
+                child: Column(
+                  spacing: 40,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    //username textbox
+                    Container(
+                      color: Colors.grey[300],
+                      padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
+                      width: 336,
+                      height: 65,
+                      child: TextField(
+                        controller: usernameController,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          hintStyle: Styles.fieldTextStyle,
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+
+                    //password textbox
+                    Container(
+                      color: Colors.grey[300],
+                      padding: EdgeInsets.fromLTRB(0, 25, 0, 5),
+                      width: 336,
+                      height: 65,
+                      child: TextField(
+                        controller: passwordController,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: Styles.fieldTextStyle,
+                          border: InputBorder.none,
+                          suffixIcon: IconButton(
+                            icon: isPasswordVisible
+                              ? Icon(Icons.visibility_off)
+                              : Icon(Icons.visibility),
+                            onPressed: () =>
+                              setState(() => isPasswordVisible = !isPasswordVisible),
+                          ),
+                        ),
+                        obscureText: isPasswordVisible,
+                      ),
+                    ),
+
+                    //Sign in button
+                    MaterialButton(
+                      onPressed: () {
+                        //temp solution; ideally would be passed as json for login endpoint or something like that
+                        print('username: ${usernameController.text}');
+                        print('password: ${passwordController.text}');
+                        
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomePage()),
+                        );
+                      },
+                      elevation: 4,
+                      color: Styles.schemeMain.secondary,
+                      shape: Styles.buttonShape,
+                      height: 70,
+                      minWidth: 350,
+                      child: const Text(
+                        'Sign In',
+                        style: Styles.buttonTextStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+        /*
         child: Container(
           height: 50,
           child: ElevatedButton(
@@ -33,6 +134,8 @@ class _LoginPageState extends State<LoginPage> {
             },
           )
         ),
+        */
+              
       ),
     );
   }
