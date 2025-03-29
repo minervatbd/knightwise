f9fd5cf9b738296465e70336ea48e588cdb38203
minervatbd <minervatbd@gmail.com>
2025-03-29 13:44:39 -0400

    tweak layout of calls and comments

--- mobile_test/lib/calls.dart
+++ mobile_test/lib/calls.dart
@@ -1,22 +1,22 @@
-// api calls here
-
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:mobile_test/models.dart';
 
 String uri = "http://www.dirediredocks.xyz/api/";
 
-// returns json list of problems of topic
+// returns list of problems of topic
 Future<List<Problem>> fetchProblems(String topic) async {
   final response = await http.get(
     Uri.parse("${uri}test/topic/$topic")
   );
 
   if (response.statusCode == 200) {
+    // decode the json response
     var p = jsonDecode(response.body);
+    // build a list of problem objects
     var problems = List<Problem>.empty(growable: true);
     for (int i = 0; i < p.length; i++) {
+      // throw each problem json into the factory to add to the problemlist
       problems.add(Problem.fromJson(p[i]));
     }
     return problems;
