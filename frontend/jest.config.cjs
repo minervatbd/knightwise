9ff499d52b4068e69f63efd254d98cffc5dca83b
YoniWorks <yeshin881@gmail.com>
2025-03-29 00:24:07 -0400

    setting for unit test

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+      '\\.(css|scss)$': 'identity-obj-proxy',
+    },
+  };
+  
