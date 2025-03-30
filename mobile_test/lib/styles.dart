646c25e0c0aa7dd3ed377a49c5f95b77afda2851
AmaryllisGreen <daytonchawk@gmail.com>
2025-03-30 17:44:59 -0400

    Update styles.dart

--- mobile_test/lib/styles.dart
+++ mobile_test/lib/styles.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  // main color scheme for ThemeData 
+  // main color scheme for ThemeData
   static const schemeMain = ColorScheme(
-    brightness: Brightness.light, 
-    primary: Color.fromRGBO(0, 0, 0, 1), 
-    onPrimary: Color.fromRGBO(255, 201, 4, 1),
-    secondary:  Color.fromRGBO(255, 201, 4, 1),
-    onSecondary: Color.fromRGBO(0, 0, 0, 1), 
-    tertiary: Color(0xFFB1B1B1),
-    onTertiary: Color.fromRGBO(0, 0, 0, 1),
-    error: Color.fromARGB(255, 255, 0, 0),
-    onError: Color.fromARGB(255, 255, 98, 98),
-    surface: Color.fromRGBO(236, 236, 236, 1),
-    onSurface: Color.fromRGBO(255, 201, 4, 1),
+      brightness: Brightness.light,
+      primary: Color.fromRGBO(0, 0, 0, 1),
+      onPrimary: Color.fromRGBO(0, 0, 0, 1),
+      secondary: Color.fromRGBO(255, 201, 4, 1),
+      onSecondary: Color(0xFFB1B1B1),
+      error: Color.fromARGB(255, 255, 0, 0),
+      onError: Color.fromARGB(255, 255, 98, 98),
+      surface: Color.fromRGBO(236, 236, 236, 1),
+      onSurface: Color.fromRGBO(255, 201, 4, 1)
   );
 
-  // specifically the color we give for correct answer feedback
   static const correctColor = Colors.green;
 
-  // text style for login/signup buttons 
-  static final buttonTextStyle = TextStyle(
+  // text style for login/signup buttons
+  static const buttonTextStyle = TextStyle(
     fontSize: 30,
     fontFamily: 'Mulish',
     fontWeight: FontWeight.bold,
-    color: schemeMain.onSecondary,
+    color: Colors.black,
   );
 
   // small text style for login/signup pages
@@ -35,8 +32,15 @@ abstract class Styles {
     color: Colors.black,
   );
 
-  
-    // small text for hyperlinks for login/signup pages
+  // small text style for login/signup pages
+  static final smallBoldTextStyle = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  // small text for hyperlinks for login/signup pages
   static const linkSmallTextStyle = TextStyle(
     fontSize: 20,
     fontFamily: 'Mulish',
@@ -45,7 +49,7 @@ abstract class Styles {
   );
 
   // text style for text field hints
-  static final fieldTextStyle = TextStyle(
+  static const fieldTextStyle = TextStyle(
     fontSize: 24,
     fontFamily: 'Mulish',
     fontWeight: FontWeight.bold,
@@ -68,14 +72,20 @@ abstract class Styles {
     color: Colors.black,
   );
 
-  //general lightweight text
+  // large text style for general headers
+  static const headerTextStyle = TextStyle(
+    fontSize: 50,
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
   static const generalTextStyle = TextStyle(
     fontSize: 24,
     fontFamily: 'Mulish',
     color: Colors.black,
   );
 
-  //text for hyperlinks
   static const linkTextStyle = TextStyle(
     fontSize: 24,
     fontFamily: 'Mulish',
@@ -85,7 +95,8 @@ abstract class Styles {
 
   // shape for login/signup buttons
   static final buttonShape = RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(10)
+      borderRadius: BorderRadius.circular(10),
+
   );
 
   // button style for login (yellow)
@@ -96,7 +107,7 @@ abstract class Styles {
   );
 
   static final grayButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: schemeMain.tertiary,
+      backgroundColor: schemeMain.onSecondary,
       shape: buttonShape,
       textStyle: buttonTextStyle
   );
