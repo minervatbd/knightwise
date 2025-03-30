b89679692b768d43fc8a931628b83e04b6cf6076
AmaryllisGreen <daytonchawk@gmail.com>
2025-03-30 17:52:57 -0400

    Update progress.dart
    adds progress bar addable and column for that

--- mobile_test/lib/pages/home/progress.dart
+++ mobile_test/lib/pages/home/progress.dart
@@ -9,11 +9,79 @@ class ProgressPage extends StatefulWidget {
 }
 
 class _ProgressPageState extends State<ProgressPage> {
+
+  progressBar(String topic, double progress, double width){
+    return Column(spacing: 3, children: [
+      SizedBox(width: width, child: Text(topic, style: Styles.smallBoldTextStyle, textAlign: TextAlign.start,)),
+      Stack(children: [
+        SizedBox(height: 40, width: width, child: Material(
+          elevation: 4,
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        )),
+        SizedBox(height: 40, width: progress*(width/100), child: Material(
+          elevation: 4,
+          color: Styles.schemeMain.secondary,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        )),
+
+      ]),
+    ]);
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    double scrnWidth = MediaQuery.of(context).size.width;
+    double containerWidth = (scrnWidth- 65);
+    bool messageReturned = false;
+    bool graphReturned = true;
+    bool problemHistoryReturned = false;
+
+
     return Center(
-      child: Text("good job on the progress :)",
-        style: Styles.buttonTextStyle,)
+      child: ListView(
+        children: <Widget>[
+        //Container for heading timer section
+          Container(
+            width: scrnWidth,
+            height: 152,
+
+            decoration: BoxDecoration(
+              color: Colors.grey.shade400,
+              boxShadow: [BoxShadow(
+                offset: Offset(0, 5),
+                blurRadius: 5,
+                spreadRadius: 5,
+                color: Colors.grey.shade400,
+              )],
+            ),
+            padding: EdgeInsets.symmetric(vertical: 35.0),
+            child: Text('My Progress', style: Styles.headerTextStyle, textAlign: TextAlign.center,),
+          ),
+          SizedBox(height: 30,),
+          if(messageReturned)
+            Container(padding: EdgeInsets.all(20), child: Container(
+              width: containerWidth,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Styles.schemeMain.secondary,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            )),
+          if(graphReturned)
+            Column(spacing: 30,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                progressBar('Algorithm Analysis', 40, containerWidth),
+              ],
+            ),
+          if(problemHistoryReturned)
+            Column(),
+          //Bar
+        ],
+      ),
     );
   }
 }
