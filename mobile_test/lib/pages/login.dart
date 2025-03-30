ebb22858072ba402a3446daa56f9749f6567abdf
AmaryllisGreen <daytonchawk@gmail.com>
2025-03-30 17:39:12 -0400

    Update login.dart
    made login functionality and some popups not finished yet

--- mobile_test/lib/pages/login.dart
+++ mobile_test/lib/pages/login.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
+import 'register.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_test/overlays.dart';
-import 'package:mobile_test/pages/home/homepage.dart';
+import 'package:knight_prep/calls.dart';
+import 'package:knight_prep/models.dart';
+import 'package:knight_prep/services.dart';
+import '../overlays.dart';
+import '../pages/home/homepage.dart';
 import '../styles.dart';
-import 'package:mobile_test/calls.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
+
+import 'package:http/http.dart' as http;
 
 final buttonStyle = Styles.yellowButtonStyle;
 
@@ -17,23 +25,345 @@ class _LoginPageState extends State<LoginPage> {
 
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+  final passwordResetController = TextEditingController();
+  final passwordConfirmController = TextEditingController();
+  final emailController = TextEditingController();
+  final otpController = TextEditingController();
+  String message = '';
   bool isPasswordVisible = true;
+  bool isResetPasswordVisible = true;
+  bool isConfirmPasswordVisible = true;
 
   @override
-  // void initState(){
-  //   super.initState();
-  //
-  //   usernameController.addListener(() => setState(() {}));
-  //   passwordController.addListener(() => setState(() {}));
-  // }
+  void initState() {
+    super.initState();
+    usernameController.addListener(() => setState(() {}));
+    passwordController.addListener(() => setState(() {}));
+    emailController.addListener(() => setState(() {}));
+    otpController.addListener(() => setState(() {}));
+    passwordResetController.addListener(() => setState(() {}));
+    passwordConfirmController.addListener(() => setState(() {}));
+  }
+
+  /*
+  @override
   void dispose() {
-    usernameController.dispose();
-    passwordController.dispose();
+    otpController.dispose();
+    emailController.dispose();
     super.dispose();
   }
+   */
+
+
+
+
+  Future openDialog() => showDialog(
+    context: context,
+    builder: (context) => StatefulBuilder(builder: (context, setState) => AlertDialog(
+
+      title: Text('Reset Password', style: Styles.timeTextStyle, textAlign: TextAlign.center,),
+      content: Column(mainAxisAlignment: MainAxisAlignment.start, spacing: 30, children: [
+        Container(
+          color: Colors.grey[300],
+          padding: EdgeInsets.fromLTRB(10, 25, 10, 0),
+          width: 300,
+          height: 65,
+          child: TextField(
+            controller: emailController,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Styles.schemeMain.primary,
+              fontFamily: 'Mulish',
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(
+                  left: 4.0,
+                  bottom: 4.0,
+                  top: 4.0
+              ),
+              hintText: 'Email',
+              hintStyle: Styles.fieldTextStyle,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        MaterialButton(
+          onPressed: () async {
+            try {
+              SendOtp sentCode = await sendEmailCode(emailController.text, "reset");
+              message = sentCode.message;
+              setState(() {});
+            }catch (e){
+              print(e);
+            }
+          },
+          elevation: 4,
+          color: Styles.schemeMain.secondary,
+          shape: Styles.buttonShape,
+          height: 60,
+          minWidth: 265,
+          child: const Text(
+            'Send Code',
+            style: Styles.buttonTextStyle,
+          ),
+        ),
+        Container(
+          color: Colors.grey[300],
+          padding: EdgeInsets.fromLTRB(10, 25, 10, 0),
+          width: 300,
+          height: 65,
+          child: TextField(
+            controller: otpController,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Styles.schemeMain.primary,
+              fontFamily: 'Mulish',
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(
+                  left: 4.0,
+                  bottom: 4.0,
+                  top: 4.0
+              ),
+              hintText: 'Verification Code',
+              hintStyle: Styles.fieldTextStyle,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        MaterialButton(
+          onPressed: () async {
+            try {
+              Verify verification = await verifyEmail(emailController.text, otpController.text);
+              message = verification.message;
+              setState(() {});
+            }catch (e){
+              print(e);
+            }
+          },
+          elevation: 4,
+          color: Styles.schemeMain.secondary,
+          shape: Styles.buttonShape,
+          height: 60,
+          minWidth: 265,
+          child: const Text(
+            'Verify Email',
+            style: Styles.buttonTextStyle,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Text(message, textAlign: TextAlign.center, style: Styles.generalTextStyle,),
+        ),
+        MaterialButton(
+          onPressed: () async {
+
+            final email = emailController.text.trim();
+            final otp = otpController.text.trim();
+
+            if (email.isEmpty || otp.isEmpty) {
+              message = 'Please fill out all fields';
+              setState(() {});
+              return;
+            }
+
+            // check email verification
+            final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+            if (!emailRegex.hasMatch(email)) {
+              message = 'Invalid email format';
+              setState(() {});
+              return;
+            }
+
+            if(message != 'Verify') {
+              message = 'Email not Verified';
+              return;
+            }
+
+            Navigator.of(context).pop(otpController.text);
+            Navigator.of(context).pop(emailController.text);
+            otpController.clear();
+            openPasswordDialog();
+          },
+          elevation: 4,
+          color: Styles.schemeMain.secondary,
+          shape: Styles.buttonShape,
+          height: 60,
+          minWidth: 265,
+          child: const Text(
+            'Reset Password',
+            style: Styles.buttonTextStyle,
+          ),
+        ),
+      ]),
+      /*
+      actions: [
+        TextButton(
+          onPressed: () => {
+            Navigator.of(context).pop(otpController.text),
+            Navigator.of(context).pop(emailController.text),
+            otpController.clear(),
+            openPasswordDialog(),
+          },
+          child: Text('Reset Password', style: Styles.buttonTextStyle,),)
+      ],
+
+       */
+    ),
+  ));
+
+  Future openPasswordDialog() => showDialog(
+    context: context,
+    builder: (context) => StatefulBuilder(builder: (context, setState) => AlertDialog(
+
+      title: Text('Reset Password', style: Styles.timeTextStyle, textAlign: TextAlign.center,),
+      content: Column(mainAxisAlignment: MainAxisAlignment.start, spacing: 30, children: [
+        Container(
+          color: Colors.grey[300],
+          padding: EdgeInsets.fromLTRB(10, 25, 10, 0),
+          width: 300,
+          height: 65,
+          child: TextField(
+            controller: passwordResetController,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Styles.schemeMain.primary,
+              fontFamily: 'Mulish',
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(
+                  left: 4.0,
+                  bottom: 4.0,
+                  top: 4.0
+              ),
+              hintText: 'Reset Password',
+              hintStyle: Styles.fieldTextStyle,
+              border: InputBorder.none,
+
+              suffixIcon: IconButton(
+                icon: isResetPasswordVisible
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.visibility),
+                color: Styles.schemeMain.primary,
+                onPressed: () =>
+                    setState(() =>
+                    isResetPasswordVisible = !isResetPasswordVisible),
+              ),
+            ),
+            obscureText: isResetPasswordVisible,
+          ),
+        ),
+        FlutterPwValidator(
+          controller: passwordResetController,
+          minLength: 5,
+          uppercaseCharCount: 1,
+          lowercaseCharCount: 1,
+          numericCharCount: 1,
+          specialCharCount: 1,
+          width: 300,
+          height: 150,
+          onSuccess: () {
+            print("Password is valid");
+          },
+          onFail: () {
+            print("Password is not valid");
+          },
+        ),
+        Container(
+          color: Colors.grey[300],
+          padding: EdgeInsets.fromLTRB(10, 25, 10, 0),
+          width: 300,
+          height: 65,
+          child: TextField(
+            controller: passwordConfirmController,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Styles.schemeMain.primary,
+              fontFamily: 'Mulish',
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.only(
+                  left: 4.0,
+                  bottom: 4.0,
+                  top: 4.0
+              ),
+              hintText: 'Confirm Password',
+              hintStyle: Styles.fieldTextStyle,
+              border: InputBorder.none,
+              suffixIcon: IconButton(
+                icon: isConfirmPasswordVisible
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.visibility),
+                color: Styles.schemeMain.primary,
+                onPressed: () =>
+                    setState(() =>
+                    isConfirmPasswordVisible = !isConfirmPasswordVisible),
+              ),
+            ),
+            obscureText: isConfirmPasswordVisible,
+          ),
+        ),
+        MaterialButton(
+          onPressed: () async {
+            try {
+
+              final password = passwordResetController.text;
+              final confirmPassword = passwordConfirmController.text;
+              // check empty field
+              if (password.isEmpty || confirmPassword.isEmpty) {
+                message = 'Please fill out all fields';
+                setState(() {});
+                return;
+              }
+
+              // check password
+              if (password != confirmPassword) {
+                message = 'Passwords do not match';
+                setState(() {});
+                return;
+              }
+
+              ResetPassword passwordReset = await resetPassword(emailController.text,passwordResetController.text);
+              message = passwordReset.message;
+
+              setState(() {});
+            }catch (e){
+              print(e);
+            }
+          },
+          elevation: 4,
+          color: Styles.schemeMain.secondary,
+          shape: Styles.buttonShape,
+          height: 60,
+          minWidth: 265,
+          child: const Text(
+            'Send Code',
+            style: Styles.buttonTextStyle,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Text(message, textAlign: TextAlign.center, style: Styles.generalTextStyle,),
+        ),
+      ]),
+    )),
+  );
+
+  CurrentUser currUser = CurrentUser();
 
   @override
   Widget build(BuildContext context) {
+    double scrnWidth = MediaQuery.of(context).size.width;
+    double containerWidth = (scrnWidth- 65);
+
     return Scaffold(
       appBar: TopBarBlank(),
       bottomNavigationBar: BottomBarBlank(),
@@ -43,8 +373,7 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           spacing: 45.0,
           children: <Widget>[
-
-          //LOGIN stripe
+            //LOGIN stripe
             Material(
               elevation: 4,
               child: Container(
@@ -64,12 +393,11 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
             ),
-
             //textboxes and button
             Form(
               child: Column(
-                spacing: 40,
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   //username textbox
                   Container(
@@ -99,6 +427,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
 
+                  SizedBox(height: 40),
                   //password textbox
                   Container(
                     color: Colors.grey[300],
@@ -137,32 +466,30 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
 
+                  SizedBox(height: 40),
                   //Sign in button
                   MaterialButton(
-                    // onPressed: () {
-                    //   //temp solution; ideally would be passed as json for login endpoint or something like that
-                    //   print('username: ${usernameController.text}');
-                    //   print('password: ${passwordController.text}');
-                    //
-                    //   Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(builder: (context) => const HomePage()),
-                    //   );
-                    // },
-                    onPressed: ()  {
-                      final username = usernameController.text.trim();
-                      final password = passwordController.text;
+                    onPressed: () async {
+                      try {
+                          //print('username: ${usernameController.text}');
+                          //print('password: ${passwordController.text}');
 
-                      if (username.isEmpty || password.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Please enter both username and password'),
-                            backgroundColor: Colors.red,
-                            behavior: SnackBarBehavior.floating,
-                            duration: Duration(seconds: 2),
-                          ),
-                        );
-                        return;
+                          var login = await createLogin(usernameController.text, passwordController.text);
+                          print(login.token);
+                          print(login.message);
+                          print(login.user.firstName);
+
+                          currUser.token = login.token;
+                          currUser.firstName = login.user.firstName;
+                          currUser.lastName = login.user.lastName;
+                          currUser.id = login.user.id;
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          );
+                      }on Exception catch (e){
+                        print('$e');
                       }
                     },
                     elevation: 4,
@@ -170,10 +497,45 @@ class _LoginPageState extends State<LoginPage> {
                     shape: Styles.buttonShape,
                     height: 70,
                     minWidth: 350,
-                    child: Text(
+                    child: const Text(
                       'Sign In',
                       style: Styles.buttonTextStyle,
                     ),
+                  ),
+              Center(child: Container(
+                  width: scrnWidth,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(children: [
+                    Text(
+                        'No account?',
+                        style: Styles.smallTextStyle,
+                      ),
+                      TextButton(
+                        onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const RegisterPage()),
+                          )},
+                        child: Text('Sign up here.', style: Styles.linkSmallTextStyle, textAlign: TextAlign.start,)
+                      ),
+                    ]),
+                  )),
+
+                  Center(child: Container(
+                    width: scrnWidth,
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(children: [
+                      Text(
+                        'Forgot Password?',
+                        style: Styles.smallTextStyle,
+                      ),
+                      TextButton(
+                        onPressed: () async => {
+                          await openDialog(),
+                        },
+                        child: Text('Reset Password', style: Styles.linkSmallTextStyle,)
+                      ),
+                    ])),
                   ),
                 ],
               ),
