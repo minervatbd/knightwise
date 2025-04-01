b3e0021256c6921c332d428538e0fbf9a435d8a5
AmaryllisGreen <daytonchawk@gmail.com>
2025-04-01 13:17:13 -0400

    Update calls.dart
    adds getProgress method, which calls the /progress/graph api call

--- mobile_test/lib/calls.dart
+++ mobile_test/lib/calls.dart
@@ -151,6 +151,27 @@ void postAnswer(Answer answer) async {
   }
 }
 
+Future<Progress> getProgress(String token) async {
+  final response = await http.get(
+    Uri.parse('${uri}progress/graph'),
+    headers: <String, String>{
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+  if (response.statusCode == 200) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Progress.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    final resBody = jsonDecode(response.body);
+    throw Exception(resBody['message'] ?? 'getProgress failed');
+  }
+}
+
+
 // Sends new user data to server
 Future <http.Response> postData({
   required String firstName,
